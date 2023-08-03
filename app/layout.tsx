bb40a1616f6b7e404b2a90321314100df8e92d0f
shaulov <shineginn@gmail.com>
2023-08-03 19:09:44 +0300

    Add MediaItem component for songs display in Your Library section

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import UserProvider from '@/providers/UserProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import Sidebar from '@/components/Sidebar'
+import getSongsByUserId from '@/actions/getSongsByUserId'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -14,11 +15,15 @@ export const metadata: Metadata = {
   description: 'Listen to music',
 }
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const userSongs = await getSongsByUserId();
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -26,7 +31,9 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <Sidebar>{children}</Sidebar>
+            <Sidebar songs={userSongs}>
+              {children}
+            </Sidebar>
           </UserProvider>
         </SupabaseProvider>
       </body>
---
+++ components/MediaItem.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import Image from "next/image";
+import useLoadImage from "@/hooks/useLoadImage";
+import { Song } from "@/types";
+
+interface MediaItemProps {
+  data: Song;
+  onClick: (id: string) => void;
+}
+
+function MediaItem({ data, onClick }: MediaItemProps) {
+  const imageUrl = useLoadImage(data);
+
+  const handleClick = () => {
+    if (onClick) {
+      return onClick(data.id);
+    }
+  }
+
+  return (
+    <article
+      className="
+        grid grid-cols-[48px_auto] items-center gap-x-3
+        w-full p-2
+        rounded-md
+        cursor-pointer
+        hover:bg-neutral-800/50
+      "
+    >
+      <h3 className="text-white truncate">{data.title}</h3>
+      <p className="text-sm text-neutral-400 truncate">{data.author}</p>
+      <div
+        className="
+          row-span-2
+          relative order-first
+          min-w-[48px] min-h-[48px]
+          overflow-hidden
+          rounded-md
+        "
+      >
+        <Image 
+          className="object-cover" 
+          src={imageUrl || '/images/liked.png'} 
+          fill 
+          alt="Media item"
+        />
+      </div>
+    </article>
+  );
+}
+
+export default MediaItem;
