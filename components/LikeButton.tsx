0cf81068971f1d63a2c982d387bc3760889e2260
shaulov <shineginn@gmail.com>
2023-08-07 23:24:15 +0300

    Add favorites functionality and fix mistakes

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSessionContext } from "@supabase/auth-helpers-react";
+import { toast } from 'react-hot-toast';
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
@@ -24,7 +25,7 @@ function LikeButton({ songId }: LikeButtonProps) {
       const { data, error} = await supabaseClient
         .from('liked_songs')
         .select('*')
-        .eq('user_id', user?.id)
+        .eq('user_id', user.id)
         .eq('song_id', songId)
         .single();
       
@@ -34,20 +35,43 @@ function LikeButton({ songId }: LikeButtonProps) {
     fetchData();
   }, [songId, supabaseClient, user?.id]);
 
-  const handleLikeClick = () => {
+  const handleLikeClick = async () => {
     if (!user) return authModal.onOpen();
 
-    console.log();
-    
+    if (isLiked) {
+      const { error } = await supabaseClient
+        .from('liked_songs')
+        .delete()
+        .eq('user_id', user.id)
+        .eq('song_id', songId);
+      
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(false);
+      }
+    } else {
+      const { error } = await supabaseClient
+        .from('liked_songs')
+        .insert({ user_id: user.id, song_id: songId });
+      
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(true);
+        toast.success('Added to Liked Song');
+      }
+    }
+
     router.refresh();
   }
 
   const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
 
   return (
-    <button onClick={handleLikeClick}>
+    <button className="transition hover:opacity-75" onClick={handleLikeClick}>
       <span className="sr-only">Like song</span>
-      <Icon size={25} />
+      <Icon color={isLiked ? '#22c55e' : 'white'} size={25} />
     </button>
   );
 }
