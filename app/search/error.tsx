1dc0dc3e0caa8a7fb356ff426d73cdec850d20ff
shaulov <shineginn@gmail.com>
2023-09-05 14:26:05 +0300

    Add loading and error components

---
+++ app/search/error.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Box from "@/components/Box";
+
+function Error () {
+  return (
+    <Box className="flex justify-center items-center h-full">
+      <p className="text-neutral-400">Something went wrong.</p>
+    </Box>
+  );
+}
+
+export default Error;
