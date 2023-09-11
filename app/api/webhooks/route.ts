ba2ca74cb78bcb48ac07db7ce3074588607379c0
shaulov <shineginn@gmail.com>
2023-09-11 16:03:08 +0300

    Fix misspelling

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -19,13 +19,14 @@ export async function POST(request: Request) {
   const body = await request.text();
   const sig = headers().get('Stripe-Signature');
 
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const webhookSecret =
+      process.env.STRIPE_WEBHOOK_SECRET_LIVE ??
+      process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
 
   try {
     if (!sig || !webhookSecret) return;
-    
-    event = stripe.webhooks.constructEvent(sig, webhookSecret, body);
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
   } catch(error: any) {
     console.log(`Error message: ${error.message}`);
     return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
